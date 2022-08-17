aea72e5ca6229739517d743f5af9a282b398766f
Camille Faulkner <camillecfaulkner@gmail.com>
2022-08-17 09:17:43 -0500

    commit

--- src/Rare.js
+++ src/Rare.js
@@ -6,6 +6,7 @@ import { NavBar } from "./components/nav/NavBar"
 export const Rare = () => {
   const [token, setTokenState] = useState(localStorage.getItem('auth_token'))
   const [userId, setUserIdState] = useState(localStorage.getItem('user_id'))
+  const [staff, setStaffState] = useState(localStorage.getItem('is_staff'))
 
   const setToken = (newToken) => {
     localStorage.setItem('auth_token', newToken)
@@ -17,8 +18,13 @@ export const Rare = () => {
     setUserIdState(userId)
   }
 
+    const setStaff = (staff) => {
+    localStorage.setItem('is_staff', staff)
+    setStaffState(staff)
+  }
+
   return <>
-    <NavBar token={token} setToken={setToken} />
-    <ApplicationViews token={token} setToken={setToken} setUserId={setUserId} userId={userId} />
+    <NavBar token={token} setToken={setToken} setStaff={setStaff}/>
+    <ApplicationViews token={token} setToken={setToken} setUserId={setUserId} userId={userId} setStaff={setStaff}/>
   </>
 }
--- src/components/profile/ProfileList.js
+++ src/components/profile/ProfileList.js
@@ -9,13 +9,23 @@ export const ProfileList = (props) => {
         getProfiles().then(data => setProfiles(data))
     }, [])
 
+    const userType = (user) => {
+        if (user.is_staff === true) {
+            return <>Admin</>
+        } else {
+            return <>Author</>
+        }
+    }
+
     return (
         <article className="profiles">
             {
-                profiles.map(profile => {
+                profiles.sort(((a, b) => { return a.user.username.localeCompare(b.user.username) })).map(profile => {
                     return <section key={`profile--${profile.id}`} className="profile">
-                        <div className="profile__fullName">{profile.user.first_name} {profile.user.last_name}</div>
-                        <div className="profile__userName">{profile.user.username}</div>
+                        <div className="profile__fullName">Full Name: {profile.user.first_name} {profile.user.last_name}</div>
+                        <div className="profile__userName">Username: {profile.user.username}</div>
+                        <div className="profile__userType">User Type: {userType(profile.user)}</div>
+                        <br/>
                     </section>
                 })
             }
