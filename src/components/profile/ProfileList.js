a6eda4fc0f2eb770f5720310e302ef951150f4e2
Michael O'Brien <michael.obrien45@gmail.com>
2022-08-17 14:01:55 -0500

    client side prof details

--- src/components/profile/ProfileList.js
+++ src/components/profile/ProfileList.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react"
 import { useState } from "react"
+import { Link } from "react-router-dom"
 import { getProfiles } from "../../managers/ProfileManager"
 
 export const ProfileList = (props) => {
-    const [ profiles, setProfiles ] = useState([])
+    const [profiles, setProfiles] = useState([])
 
     useEffect(() => {
         getProfiles().then(data => setProfiles(data))
@@ -11,13 +12,14 @@ export const ProfileList = (props) => {
 
     return (
         <article className="profiles">
+
             {
                 profiles.map(profile => {
                     return <section key={`profile--${profile.id}`} className="profile">
                         <div className="profile__fullName">{profile.user.first_name} {profile.user.last_name}</div>
-                        <div className="profile__userName">{profile.user.username}</div>
+                        <Link to={`/profiles/${profile.id}`} className="button is-Link is-light">{profile.user.username}</Link>
                     </section>
-                })
+                    })
             }
         </article>
     )
