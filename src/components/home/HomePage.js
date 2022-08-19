d358f1476ba2909607de56c8b43d9dcfbb607166
Ryley Hynes <ryleyhynes@gmail.com>
2022-08-19 14:59:33 -0500

    added homepage posts

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -7,14 +7,29 @@ const [posts, setPosts] = useState([])
 
 
 
+
 useEffect(() => {
-    getSubscriptionsByUser(localStorage.getItem("user_id")).then(data => setPosts(data))
-}, [])
+    getSubscriptionsByUser(localStorage.getItem("user_id")).then(data => { 
+        let posts = []
+        let postArray = data.map(subscription => subscription.author.posts)
+        for (let postList of postArray) {
+            for (let post of postList) {
+                posts.push(post)
+            }
+        }
+        setPosts(posts)
+})}, [])
+
+
 return (
     <>
     {
         posts.map(post => {
-            <div>{post.title}</div>
+            return <>
+            <div>Title: {post?.title}</div>
+            <div>Publication Date: {post.publication_date}</div>
+            <div>Content: {post.content}</div>
+            </>
         })
     }
     </>
